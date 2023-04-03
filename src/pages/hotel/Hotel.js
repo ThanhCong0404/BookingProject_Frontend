be4bcfe20accf701cb094fdf8a1fa5fdf5d886bf
ThanhCong0404 <Thanhcong04042001@gmail.com>
2023-04-03 10:27:19 +0700

    reach fetch data using URl Location useLocation() for hotel detail

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -1,22 +1,22 @@
 import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
+import { useLocation } from "react-router-dom"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
+import useFetch from "../../hooks/useFetch"
 import "./Hotel.css"
 
 const Hotel = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
   const [slideNumber,setSlideNumber] = useState(0);
   const [open,setOpen] = useState(false);
 
-  const photos = [
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {src: "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-  ]
+  const {data,loading,error,reFetch} = useFetch(`/hotels/find/${id}`); //proxy root config in packake.json
+
+
+ 
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -26,9 +26,9 @@ const Hotel = () => {
   const handleMove = (direction) => {
     let newSlideNumber;
     if(direction === "l"){
-      newSlideNumber = slideNumber === 0 ? (photos.length-1) : slideNumber-1;
+      newSlideNumber = slideNumber === 0 ? (data.photos.length-1) : slideNumber-1;
     }else{
-      newSlideNumber = slideNumber === (photos.length-1) ? 0 : slideNumber+1;
+      newSlideNumber = slideNumber === (data.photos.length-1) ? 0 : slideNumber+1;
     }
 
     setSlideNumber(newSlideNumber);
@@ -38,13 +38,13 @@ const Hotel = () => {
     <div>
       <Navbar/>
       <Header type="list"/>
-      <div className="hotelContainer">
+      { loading ? "loading..." : <div className="hotelContainer">
         { open && <div className="slider">
 
           <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={()=> setOpen(false)}/>
           <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={()=>handleMove("l")}/>
           <div className="sliderWrapper">
-            <img src={photos[slideNumber].src} className="sliderImg"/>
+            <img src={data.photos[slideNumber]} className="sliderImg"/>
 
           </div>
           <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={()=>handleMove("r")}/>
@@ -52,38 +52,32 @@ const Hotel = () => {
 
         <div className="hotelWrapper">
           <button className="bookNow">Đặt ngay</button>
-          <h1 className="hotelTitle">Dalat Wind Hotel</h1>
+          <h1 className="hotelTitle">{data.name}</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot}/>
-            <span>DaLat</span>
+            <span>{data.address}</span>
             
           </div>
           <span className="hotelDistance">
-            Vị trí tuyệt vời - Hiển thị bản đồ 
+            Vị trí tuyệt vời - Cách trung tâm {data.distance}m 
 
           </span>
           <span className="hotelPriceHighlight"> 
-            Nhanh tay đặt ngay với giá hấp dẫn
+            Nhanh tay đặt ngay với giá hấp dẫn chỉ từ {data.cheapestPrice}
           </span>
           <div className="hotelImages">
-            {photos.map((photo,index) => (
+            {data.photos?.map((photo,index) => (
               <div className="hotelImgWrapper">
-                <img onClick={()=>handleOpen(index)} src={photo.src} alt="" className="hotelImg" />
+                <img onClick={()=>handleOpen(index)} src={photo} alt="" className="hotelImg" />
               </div>
             ))}
           </div>
 
           <div className="hotelDetails">
             <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle"> Nomad Home Đà Lạt đã chào đón khách từ 7 tháng 11 2022. </h1>
+              <h1 className="hotelTitle"> {data.title} </h1>
               <p className="hotelDesc">
-              Situated 3.1 km from Dalat Palace Golf Club, Nomad Home Đà Lạt features accommodation with free WiFi and free private parking.
-
-              There is a private bathroom with shower in every unit, along with free toiletries, a hairdryer and slippers.
-
-              Guests can relax in the garden at the property.
-
-              Lam Vien Square is 3.4 km from the homestay, while Xuan Huong Lake is 3.5 km from the property. The nearest airport is Lien Khuong Airport, 30 km from Nomad Home Đà Lạt.
+                {data.desc}
               </p>
             </div>
             <div className="hotelDetailsPrice">
@@ -99,7 +93,7 @@ const Hotel = () => {
           </div>
         </div>
 
-      </div>
+      </div>}
     </div>
   )
 }
